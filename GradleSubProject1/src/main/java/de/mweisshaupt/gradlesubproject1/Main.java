d0b0d21eb4f38b53e23e1b3446d09414793e45f0
Martin Weißhaupt <mweisshaupt@mweisshaupt.de>
2019-01-30 22:13:54 +0100

    Added a root and two subprojects

---
+++ GradleSubProject1/src/main/java/de/mweisshaupt/gradlesubproject1/Main.java
@@ -0,0 +1,9 @@
+package de.mweisshaupt.gradlesubproject1;
+
+public class Main {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    }
+}
---
+++ GradleSubProject2/src/main/java/de/mweisshaupt/gradlesubproject2/Main.java
@@ -0,0 +1,9 @@
+package de.mweisshaupt.gradlesubproject2;
+
+public class Main {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    }
+}
